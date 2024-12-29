36f477b88e4e4a84fd8ab08825ec24fa087e2f96
jack andrews <effbiae@gmail.com>
2024-12-29 19:11:37 +1100

    force unzip

--- pollk.sh
+++ pollk.sh
@@ -7,6 +7,6 @@ new   (){ $curl -I $url --header If-None-Match:\ `cat etag`|head -1|grep 200;}
 get   (){ $curl $url >k.zip;}
 clone (){ git clone $g;}
 rmed  (){ comm -23 <(cd k.edu; echo *|tr -s ' ' '\n'|sort) <(echo README.md LICENSE $(zipinfo -1 k.zip)|tr -s ' ' '\n'|sort);}
-gitit (){ (rm=$(rmed); cd k.edu; git pull; git rm $rm; unzip ../k.zip; git add .; git commit -m "$(date -u)"; git push);}
+gitit (){ (rm=$(rmed); cd k.edu; git pull; git rm $rm; unzip -o ../k.zip; git add .; git commit -m "$(date -u)"; git push);}
 if [ ! -e etag ]; then etag; clone; fi
 if new; then set -x; get; gitit; etag; fi
