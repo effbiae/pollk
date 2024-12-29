3925646d1dbe4570e89caa28ce5f159d546b04d6
jack andrews <effbiae@gmail.com>
2024-12-29 17:54:43 +1100

    initial

---
+++ pollk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+url=shakti.com/edu/k.zip
+g=git@github.com:effbiae/k.edu.git
+curl="curl -s -S"
+etag  (){ $curl -I $url |grep ETag |grep -o '".*"' >etag;}
+new   (){ $curl -I $url --header If-None-Match:\ `cat etag`|head -1|grep 200;}
+get   (){ $curl $url >k.zip;}
+clone (){ git clone $g;}
+rmed  (){ comm -23 <(cd k.edu; echo *|tr -s ' ' '\n'|sort) <(echo README.md LICENSE $(zipinfo -1 k.zip)|tr -s ' ' '\n'|sort);}
+gitit (){ (rm=$(rmed); cd k.edu; git pull; git rm $rm; unzip ../k.zip; git add .; git commit -m "$(date -u)"; git push);}
+if [ ! -e etag ]; then etag; clone; fi
+if new; then set -x; get; gitit; etag; fi
