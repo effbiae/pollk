c8f061b0b43f695412e21081da5da8b2a29312a7
jack andrews <effbiae@gmail.com>
2024-12-29 22:41:18 +1100

    remove git pull

--- pollk.sh
+++ pollk.sh
@@ -7,7 +7,7 @@ new   (){ $curl -I $url --header If-None-Match:\ `cat etag`|head -1|grep 200;}
 get   (){ $curl $url >k.zip;}
 clone (){ git clone $g;}
 rmed  (){ comm -23 <(cd k.edu; echo *|tr -s ' ' '\n'|sort) <(echo $(zipinfo -1 k.zip)|tr -s ' ' '\n'|sort);}
-gitit (){ (rm=$(rmed); cd k.edu; git pull; git rm $rm; unzip -o ../k.zip; git add .; git commit -m "$(date -u)"; git push);}
+gitit (){ (rm=$(rmed); cd k.edu; git rm $rm; unzip -o ../k.zip; git add .; git commit -m "$(date -u)"; git push);}
 bump  (){ git add etag; git commit -m "$(date -u)"; git push; }
 if [ ! -e etag ]; then etag; clone; fi
 if new; then set -x; get; gitit; etag; bump; fi
